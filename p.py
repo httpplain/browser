471a27bacd6ce47f705bda3b7ff636c38faa9410
httpplain <147990607+httpplain@users.noreply.github.com>
2024-01-01 23:06:49 +0700

    Update p.py

--- p.py
+++ p.py
@@ -240,7 +240,7 @@ def check_proxy_http(proxy):
     }
     
     try:
-        url = 'http://httpbin.org/get' 
+        url = 'http://www.google.com' 
         r = requests.get(url, proxies=proxy_dict, timeout=5)
         if r.status_code == 200:
             with output_lock:
